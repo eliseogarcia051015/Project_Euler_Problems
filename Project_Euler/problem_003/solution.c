ffc094b32c406c9ee5137b7f3c8ad07ddf211bdf
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-09-18 23:28:26 -0500

    m

--- Project_Euler/problem_003/solution.c
+++ Project_Euler/problem_003/solution.c
@@ -3,6 +3,8 @@ The prime factors of 13195 are 5, 7, 13, and 29
 What is the largest prime factor of the number 600851475143?
 */
 
+//idea: for loop from i:sqrt(n)
 int main(){
+    long n = 600851475143;
     return 0;
 }
